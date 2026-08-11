059d4b9cd8091e6aabb71f7b273ae5b94b8bb404
Shir Studio <shirstudio20@gmail.com>
2026-08-12 01:35:14 +0300

    Fix GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
+﻿import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/dirshu2/',
   plugins: [react()],
   server: { port: 5173, open: true },
 })
